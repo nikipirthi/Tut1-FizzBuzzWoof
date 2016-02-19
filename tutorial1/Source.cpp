c63ab7adfebfd1da1abc4b3a249cbd8045b5a478
nikipirthi <nikitapirthi@gmail.com>
2016-02-19 14:59:24 +0200

    fizzbuzzwoof

--- tutorial1/Source.cpp
+++ tutorial1/Source.cpp
@@ -3,17 +3,22 @@ using namespace std;
 int main()
 {
 	int i;
-	for (i = 1; i <= 20; i++)
+	for (i = 1; i <= 50; i++)
 	{
-		
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+			cout << "fizz buzz woof" << endl;
+		else if (i % 3 == 0 && i % 5 == 0)
 			cout << "fizz buzz" << endl;
-		
+		else if (i % 5 == 0 && i % 7 == 0)
+			cout << "buzz woof" << endl;
+		else if (i % 3 == 0 && i % 7 == 0)
+			cout << "fizz woof" << endl;
 		else if (i % 3 == 0)
 			cout << "fizz" << endl;
 		else if (i % 5 == 0)
 			cout << "buzz" << endl;
-		
+		else if (i % 7 == 0)
+			cout << "woof" << endl;
 		else
 			cout << i << endl;
 	}
